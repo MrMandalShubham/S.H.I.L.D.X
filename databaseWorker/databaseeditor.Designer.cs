d026021138f0b2a53486b932a418d601b9182ad5
SHUBHAM MANDAL <mandalshubham171@gmail.com>
2022-02-23 21:52:05 +0530

    Add files via upload

---
+++ databaseWorker/databaseeditor.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace databaseworker
+{
+    partial class Databaseeditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Databaseeditor));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.text_VirusPath = new System.Windows.Forms.TextBox();
+            this.btnAddIt = new System.Windows.Forms.Button();
+            this.radioSusp = new System.Windows.Forms.RadioButton();
+            this.textSHA1 = new System.Windows.Forms.TextBox();
+            this.radioVirus = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSelectAntivirus = new System.Windows.Forms.Button();
+            this.textPathToAntivirus = new System.Windows.Forms.TextBox();
+            this.Seperator = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // btnSelectFile
+            // 
+            resources.ApplyResources(this.btnSelectFile, "btnSelectFile");
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // text_VirusPath
+            // 
+            resources.ApplyResources(this.text_VirusPath, "text_VirusPath");
+            this.text_VirusPath.Name = "text_VirusPath";
+            this.text_VirusPath.ReadOnly = true;
+            // 
+            // btnAddIt
+            // 
+            resources.ApplyResources(this.btnAddIt, "btnAddIt");
+            this.btnAddIt.Name = "btnAddIt";
+            this.btnAddIt.UseVisualStyleBackColor = true;
+            this.btnAddIt.Click += new System.EventHandler(this.btnAddIt_Click);
+            // 
+            // radioSusp
+            // 
+            resources.ApplyResources(this.radioSusp, "radioSusp");
+            this.radioSusp.Name = "radioSusp";
+            this.radioSusp.UseVisualStyleBackColor = true;
+            // 
+            // textSHA1
+            // 
+            resources.ApplyResources(this.textSHA1, "textSHA1");
+            this.textSHA1.Name = "textSHA1";
+            this.textSHA1.ReadOnly = true;
+            // 
+            // radioVirus
+            // 
+            resources.ApplyResources(this.radioVirus, "radioVirus");
+            this.radioVirus.Checked = true;
+            this.radioVirus.Name = "radioVirus";
+            this.radioVirus.TabStop = true;
+            this.radioVirus.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.radioVirus);
+            this.panel1.Controls.Add(this.radioSusp);
+            this.panel1.Name = "panel1";
+            // 
+            // btnSelectAntivirus
+            // 
+            resources.ApplyResources(this.btnSelectAntivirus, "btnSelectAntivirus");
+            this.btnSelectAntivirus.Name = "btnSelectAntivirus";
+            this.btnSelectAntivirus.UseVisualStyleBackColor = true;
+            this.btnSelectAntivirus.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textPathToAntivirus
+            // 
+            resources.ApplyResources(this.textPathToAntivirus, "textPathToAntivirus");
+            this.textPathToAntivirus.Name = "textPathToAntivirus";
+            this.textPathToAntivirus.ReadOnly = true;
+            // 
+            // Seperator
+            // 
+            resources.ApplyResources(this.Seperator, "Seperator");
+            this.Seperator.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Seperator.ForeColor = System.Drawing.Color.Gray;
+            this.Seperator.Name = "Seperator";
+            // 
+            // Databaseeditor
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Seperator);
+            this.Controls.Add(this.textPathToAntivirus);
+            this.Controls.Add(this.btnSelectAntivirus);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textSHA1);
+            this.Controls.Add(this.btnAddIt);
+            this.Controls.Add(this.text_VirusPath);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Databaseeditor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.databaseeditor_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox text_VirusPath;
+        private System.Windows.Forms.Button btnAddIt;
+        private System.Windows.Forms.RadioButton radioSusp;
+        private System.Windows.Forms.TextBox textSHA1;
+        private System.Windows.Forms.RadioButton radioVirus;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSelectAntivirus;
+        private System.Windows.Forms.TextBox textPathToAntivirus;
+        private System.Windows.Forms.Panel Seperator;
+    }
+}
+
---
+++ databaseWorker/databaseeditor.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace databaseworker
+{
+    public partial class Databaseeditor : Form
+    {
+        private readonly bool _usedLauncher;
+        public Databaseeditor(bool usedLauncherPool)
+        {
+            string temp = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\lang.ini");
+            var currentCulture = new CultureInfo(temp);
+            Thread.CurrentThread.CurrentCulture = currentCulture;
+            Thread.CurrentThread.CurrentUICulture = currentCulture;
+            InitializeComponent();
+            _usedLauncher = usedLauncherPool;
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog {Title = LanguageResources.select_file};
+            open.ShowDialog();
+
+            if (open.FileName == "") return;
+            text_VirusPath.Text = open.FileName;
+            textSHA1.Text = GetSHA1(text_VirusPath.Text);
+            btnAddIt.Enabled = true;
+        }
+
+        private string _SHA;
+        private string GetSHA1(string filename)
+        {
+            using (var sha1 = SHA1.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    _SHA = BitConverter.ToString(sha1.ComputeHash(stream)).Replace("-", string.Empty);
+                    return _SHA;
+                }
+            }
+        }
+
+
+        private string _path;
+        private bool _contains;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fold = new FolderBrowserDialog {Description = LanguageResources.select_installation_folder};
+            fold.ShowDialog();
+            _contains = false;
+
+            if (fold.SelectedPath == "") return;
+            _path = fold.SelectedPath;
+            foreach (string file in Directory.GetFiles(_path))
+            {
+                if (file.Contains("InfANT.exe"))
+                {
+                    textPathToAntivirus.Text = fold.SelectedPath;
+                    btnSelectAntivirus.Enabled = false;
+                    btnSelectFile.Enabled      = true;
+                    _contains = true;
+                    File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"\lastpath.txt", _path);
+                }
+            }
+
+            if(_contains == false)
+            {
+                MessageBox.Show(LanguageResources.cant_find_infant_in_folder);
+            }
+        }
+
+        private List<string> _localDatabase;
+        private List<string> _localDatabaseSusp;
+        private void btnAddIt_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(_path + @"\localdatabase.txt"))
+                _localDatabase = File.ReadAllLines(_path + @"\localdatabase.txt", Encoding.UTF8).ToList<string>();
+            else
+                _localDatabase = new List<string>();
+
+            if (File.Exists(_path + @"\localdatabasesusp.txt"))
+                _localDatabaseSusp = File.ReadAllLines(_path + @"\localdatabasesusp.txt", Encoding.UTF8).ToList<string>();
+            else
+                _localDatabaseSusp = new List<string>();
+
+            if(radioSusp.Checked == false)
+            {
+                try
+                {
+                    if (!_localDatabase.Contains(_SHA) & !_localDatabaseSusp.Contains(_SHA))
+                    {
+                        _localDatabase.Add(_SHA);
+                        File.WriteAllLines(_path + @"\localdatabase.txt", _localDatabase);
+                    }
+                    else
+                    {
+                        MessageBox.Show(LanguageResources.this_hash_already_exists);
+                        return;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show(LanguageResources.access_denied);
+                    return;
+                }
+            }
+            else
+            {
+                try
+                {
+                    if (!_localDatabase.Contains(_SHA) & !_localDatabaseSusp.Contains(_SHA))
+                    {
+                        _localDatabase.Add(_SHA);
+                        File.WriteAllLines(_path + @"\localdatabasesusp.txt", _localDatabase);
+                    }
+                    else
+                    {
+                        MessageBox.Show(LanguageResources.this_hash_already_exists);
+                        return;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show(LanguageResources.access_denied);
+                    return;
+                }
+            }
+            MessageBox.Show(LanguageResources.done);
+            text_VirusPath.Text = LanguageResources.select_file_text;
+            textSHA1.Text = string.Empty;
+            btnAddIt.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!_usedLauncher)
+            {
+                MessageBox.Show(LanguageResources.open_launcher_instead);
+                Application.Exit();
+            }
+
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"\InfANT.exe"))
+            {
+                _path = Directory.GetCurrentDirectory();
+                textPathToAntivirus.Text = _path;
+                btnSelectFile.Enabled = true;
+                _contains = true;
+                return;
+            }
+            
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"\lastpath.txt")) return;
+            _path = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\lastpath.txt");
+
+            try
+            {
+                foreach (string file in Directory.GetFiles(_path))
+                {
+                    if (!file.Contains("InfANT.exe")) continue;
+                    textPathToAntivirus.Text = _path;
+                    btnSelectFile.Enabled = true;
+                    _contains = true;
+                }
+            }
+            catch
+            {
+                File.Delete(AppDomain.CurrentDomain.BaseDirectory + @"\lastpath.txt");
+                Application.Restart();
+                return;
+            }
+
+            if(_contains == false)
+            {
+                File.Delete(AppDomain.CurrentDomain.BaseDirectory + @"\lastpath.txt");
+                Application.Restart();
+                return;
+            }
+            _contains = false;
+        }
+
+        private void databaseeditor_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
